0696ab5ff4206b8557561d9e162133dd533e28d5
juanma1980 <juanma1980@gmail.com>
2019-08-29 08:44:58 +0200

    add python2 lib

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 #
 # $Id: setup.py,v 1.32 2010/10/17 15:47:21 ghantoos Exp $
 #
@@ -23,7 +23,7 @@ from distutils.core import setup
 
 if __name__ == '__main__':
 
-    setup(name='python3-appconfig',
+    setup(name='python-appconfig',
         version='0.1',
         description='Lliurex appconfig',
         long_description="""""",
