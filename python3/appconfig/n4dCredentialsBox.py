545150cb2bf1d4e310660b736b18300199a11ca2
juanma1980 <juanma1980@gmail.com>
2021-05-13 19:39:35 +0200

    Reimplemented as module

--- python3/appconfig/n4dCredentialsBox.py
+++ python3/appconfig/n4dCredentialsBox.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQuick import QQuickView
+from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtCore import QUrl, QObject, Slot, Signal,Property
 import os
 import subprocess
@@ -15,22 +16,42 @@ except:
 
 
 class Tunnel(QObject):
+	onQmlTicket=Signal(str)
 
 	@Slot(str)
 	def on_ticket(self, ticket):
 		ticket=ticket.replace(' ','##U+0020##').rstrip()
-		print(ticket)
-		app.quit()
+		self.onQmlTicket.emit(ticket)
+		return True
 	#def on_ticket
 
 #class Tunnel
 
-app = QApplication([_("Authentication required")])
-tunnel = Tunnel()
-view = QQuickView()
-view.rootContext().setContextProperty("tunnel", tunnel)
-url = QUrl("/usr/share/appconfig/auth/login.qml")
-view.setSource(url)
-view.show()
+class n4dCredentials(QObject):
+	onTicket=Signal(str)
+	def __init__(self):
+		super(n4dCredentials, self).__init__()
+		if len(sys.argv)==2:
+			server=sys.argv[1]
 
-sys.exit(app.exec_())
+		self.tunnel = Tunnel()
+		self.tunnel.onQmlTicket.connect(self._onTicket)
+
+	def loginBox(self,server='localhost'):
+			#self.view = QQuickView()
+		self.qview = QQmlApplicationEngine()
+		self.server=server
+		self.qview.rootContext().setContextProperty("tunnel", self.tunnel)
+		self.qview.setInitialProperties({"address": self.server})
+		url = QUrl("/usr/share/appconfig/auth/login.qml")
+		#self.view.setSource(url)
+		self.qview.load(url)
+
+	@Slot(str)
+	def _onTicket(self,*args):
+		print("A: {}".format(args))
+		ticket=args[0]
+		#self.view.close()
+		self.onTicket.emit(ticket)
+		return True
+
